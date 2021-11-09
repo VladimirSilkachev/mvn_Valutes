8f483196fcaebac7fdda94bd4c9ab7e99552e036
VladimirSilkachev <provovak@yandex.ru>
2021-11-09 19:56:30 +0700

    Initial commit

---
+++ src/main/java/com/app/Parser.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
+
+public class Parser {
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+    }
+}
