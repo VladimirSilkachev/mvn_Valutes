b3061c61f22b1a4eda91787ca636e8d064530c33
VladimirSilkachev <provovak@yandex.ru>
2021-11-10 22:37:11 +0700

    without time

--- src/main/java/com/app/Parser.java
+++ src/main/java/com/app/Parser.java
@@ -1,12 +1,2 @@
-package com.app;
-
-import kong.unirest.HttpResponse;
-import kong.unirest.JsonNode;
-import kong.unirest.Unirest;
-
-
-public class Parser {
-    public static void main(String[] argv) {
-        System.out.println("Hello world");
-    }
+package com.app;public class Parser {
 }
