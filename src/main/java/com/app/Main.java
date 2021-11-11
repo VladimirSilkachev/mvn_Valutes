9fffbcbe3b51bd8d5d74c372993f5910b67ec0c1
VladimirSilkachev <provovak@yandex.ru>
2021-11-11 10:26:47 +0700

    final

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,2 +1,43 @@
-package com.app;public class Main {
+package com.app;
+
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        String time;
+        String code;
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Для выхода напишите exit.");
+        while (true) {
+            StringBuilder url = new StringBuilder("https://www.cbr.ru/scripts/XML_daily.asp?date_req=");
+
+            System.out.println("Введите дату. Для выбора самой актуальной даты оставьте поле пустым.");
+            time = sc.nextLine();
+            time = time.replaceAll("\\.","/").replaceAll("\\s+", "/");
+            url.append(time);
+            if (time.equals("exit")){ break;}
+
+            System.out.println("Введите код валюты");
+            code = sc.nextLine().toUpperCase(Locale.ROOT);
+
+            if (code.equals("EXIT")){ break;}
+
+            if (code.length() != 3) {
+                System.out.println("Недопустимый код валюты");
+                continue;
+            }
+
+            Parser x = new Parser(url.toString(), code);
+            System.out.println(x.domParser());
+
+
+
+        }
+    }
 }
--- src/main/java/com/app/Parser.java
+++ src/main/java/com/app/Parser.java
@@ -1,2 +1,79 @@
-package com.app;public class Parser {
+package com.app;
+
+import kong.unirest.Unirest;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.*;
+import java.io.*;
+import java.util.Objects;
+
+public class Parser {
+
+    public String time;
+    public String code;
+    public String name;
+    public String value;
+
+
+    public Parser(String time, String code) throws ParserConfigurationException, IOException, SAXException {
+        this.time = time;
+        this.code = code;
+    }
+
+    public String getTime() {return time;}
+
+    public void setValue(String value){
+        this.value = value;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+    public String getBody() {
+        return Unirest.get(getTime()).asString().getBody();
+    }
+
+
+
+    public String domParser() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(getBody())));
+        document.getDocumentElement().normalize();
+        System.out.println(time);
+
+
+        NodeList nList = document.getElementsByTagName("CharCode");
+
+        int count = -1;
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node node = nList.item(temp);
+
+            if (Objects.equals(node.getTextContent(), this.code)) {
+                count = temp;
+            }
+
+        }
+        if (count == -1) {
+            return "Нет такой валюты";
+        }
+
+        NodeList nList1 = document.getElementsByTagName("Value");
+        Node value = nList1.item(count);
+        setValue(value.getTextContent());
+
+        NodeList nList2 = document.getElementsByTagName("Name");
+        Node name = nList2.item(count);
+        setName(name.getTextContent());
+
+
+        return "1 " + name.getTextContent() + " = " + value.getTextContent() + " Российских рубля";
+
+
+    }
 }
+
+
+
